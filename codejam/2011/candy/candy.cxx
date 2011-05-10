042e75616a392620e7975dfbcf151c1bd6c9c2d5
Ani Saraf <anirudhsaraf@gmail.com>
2011-05-10 16:47:24 -0400

    BotTrust / Magicka commit from Google CodeJam 2011

---
+++ codejam/2011/candy/candy.cxx
@@ -0,0 +1,35 @@
+#include <sstream>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <cstdio>
+#include <map>
+#include <iterator>
+using namespace std;
+
+
+class Candy {
+  vector
+
+
+private: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
