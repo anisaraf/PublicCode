4f76adf1eb0fb8543d6dc0b24ac674c1a322c39d
Ani Saraf <anirudhsaraf@gmail.com>
2011-07-31 16:34:32 -0400

    Working perms without C++0x

--- random/genperm.cxx
+++ random/genperm.cxx
@@ -11,25 +11,31 @@ using namespace std;
 
 vector<string> gen_perm(string s) {
 
+
   vector<string> perms;
+  if(s.length() == 1) 
+    perms.push_back(s);
+
   for(int i = 0; i < s.length(); i++) {
-    char addMe = s[i];
-    s.erase(i,1);
-    vector<string> permsTemp = gen_perm(s);
-    for_each(permsTemp.begin(), permsTemp.end(), [&](string elem) { 
-	elem.insert(elem.begin(), addMe);
-	perms.push_back(elem);
-      });
+    string t = s;
+    t.erase(i,1);
+    vector<string> permsTemp = gen_perm(t);
+    for(int j = 0 ; j < permsTemp.size(); j++) {
+      permsTemp[j].insert(permsTemp[j].begin(), s[i]);
+      perms.push_back(permsTemp[j]);
+    }
   }
   return perms;
 };
 
 
 
-int main() {
+int main(int args, char* argv[]) {
 
+  string s(argv[1]);
   vector<string> p;
-  p = gen_perm("test");
+  p = gen_perm(s);
   copy ( p.begin(), p.end(), ostream_iterator<string>(cout,"\n") );
+  cout<<"Total Perms"<<p.size()<<endl;
 
 }
